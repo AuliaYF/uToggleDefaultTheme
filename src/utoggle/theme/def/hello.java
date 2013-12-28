fbb9ef285f6dc903a0e68a34e270c851080e16ab
AuliaYF <auliayf@gmail.com>
2013-12-28 18:45:02 +0700

    Initial Commit.
    Current Status:
    - 9 Toggles
    
    Todo Lists:
    - Add MainActivity for theme details.

---
+++ src/utoggle/theme/def/hello.java
@@ -0,0 +1,11 @@
+package utoggle.theme.def;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class hello extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
